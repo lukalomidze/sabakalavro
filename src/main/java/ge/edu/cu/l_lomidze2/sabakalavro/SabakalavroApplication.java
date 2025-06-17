7f46e53a37574ecad82af28f89039090dc4e1563
Luka Lomidze <luka_lomidze@epam.com>
2025-06-17 12:18:28 +0400

    initial project

---
+++ src/main/java/ge/edu/cu/l_lomidze2/sabakalavro/SabakalavroApplication.java
@@ -0,0 +1,13 @@
+package ge.edu.cu.l_lomidze2.sabakalavro;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SabakalavroApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SabakalavroApplication.class, args);
+	}
+
+}
---
+++ src/test/java/ge/edu/cu/l_lomidze2/sabakalavro/SabakalavroApplicationTests.java
@@ -0,0 +1,13 @@
+package ge.edu.cu.l_lomidze2.sabakalavro;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SabakalavroApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
