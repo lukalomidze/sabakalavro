0ac29d9e78010fd40f77f828f1a91f8d7a405962
Luka Lomidze <luka_lomidze@epam.com>
2025-07-04 19:14:53 +0400

    removed extras

--- src/main/java/ge/edu/cu/l_lomidze2/sabakalavro/constant/Gender.java
+++
@@ -1,7 +0,0 @@
-package ge.edu.cu.l_lomidze2.sabakalavro.constant;
-
-public enum Gender {
-    MALE,
-    FEMALE,
-    NOT_STATED
-}
--- src/main/java/ge/edu/cu/l_lomidze2/sabakalavro/model/User.java
+++ src/main/java/ge/edu/cu/l_lomidze2/sabakalavro/model/User.java
@@ -1,33 +1,16 @@
 package ge.edu.cu.l_lomidze2.sabakalavro.model;
 
-import ge.edu.cu.l_lomidze2.sabakalavro.constant.Gender;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import lombok.Builder;
+import lombok.Data;
 
 @Entity
 @Builder
+@Data
 public class User {
-    public User(
-        Long id,
-        String name,
-        String surname,
-        Integer age,
-        Gender gender,
-        String username,
-        String password
-    ) {
-        this.id = id;
-        this.name = name;
-        this.surname = surname;
-        this.age = age;
-        this.gender = gender;
-        this.username = username;
-        this.password = password;
-    }
-
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
@@ -35,63 +18,7 @@ public class User {
     private String name;
     private String surname;
     private Integer age;
-    private Gender gender;
+    private String email;
     private String username;
     private String password;
-
-    public Long getId() {
-        return id;
-    }
-
-    public void setId(Long id) {
-        this.id = id;
-    }
-
-    public String getName() {
-        return name;
-    }
-
-    public void setName(String name) {
-        this.name = name;
-    }
-
-    public String getSurname() {
-        return surname;
-    }
-
-    public void setSurname(String surname) {
-        this.surname = surname;
-    }
-
-    public Integer getAge() {
-        return age;
-    }
-
-    public void setAge(Integer age) {
-        this.age = age;
-    }
-
-    public Gender getGender() {
-        return gender;
-    }
-
-    public void setGender(Gender gender) {
-        this.gender = gender;
-    }
-
-    public String getUsername() {
-        return username;
-    }
-
-    public void setUsername(String username) {
-        this.username = username;
-    }
-
-    public String getPassword() {
-        return password;
-    }
-
-    public void setPassword(String password) {
-        this.password = password;
-    }
 }
