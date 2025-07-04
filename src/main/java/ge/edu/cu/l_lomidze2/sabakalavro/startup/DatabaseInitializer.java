cb541711d95f17a451bced1589f71c96c63dd489
Luka Lomidze <luka_lomidze@epam.com>
2025-07-04 19:15:31 +0400

    encoded pass on init

--- src/main/java/ge/edu/cu/l_lomidze2/sabakalavro/startup/DatabaseInitializer.java
+++ src/main/java/ge/edu/cu/l_lomidze2/sabakalavro/startup/DatabaseInitializer.java
@@ -4,10 +4,12 @@ import java.io.IOException;
 
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.ApplicationArguments;
 import org.springframework.boot.ApplicationRunner;
 import org.springframework.core.io.ResourceLoader;
+import org.springframework.security.crypto.password.PasswordEncoder;
 
 import ge.edu.cu.l_lomidze2.sabakalavro.model.User;
 import ge.edu.cu.l_lomidze2.sabakalavro.repository.UserRepository;
@@ -16,17 +18,14 @@ import ge.edu.cu.l_lomidze2.sabakalavro.util.JsonUtil;
 public class DatabaseInitializer implements ApplicationRunner {
     private static Logger logger = LogManager.getLogger();
 
+    @Autowired
+    private PasswordEncoder encoder;
+
+    @Autowired
     private UserRepository userRepository;
 
+    @Autowired
     private ResourceLoader resourceLoader;
-
-    public DatabaseInitializer(
-        UserRepository userRepository,
-        ResourceLoader resourceLoader
-    ) {
-        this.userRepository = userRepository;
-        this.resourceLoader = resourceLoader;
-    }
 
     @Value("${app.startup.data.path}")
     private String jsonPath;
@@ -54,6 +53,8 @@ public class DatabaseInitializer implements ApplicationRunner {
             json,
             User.class
         );
+
+        users.stream().forEach(user -> user.setPassword(encoder.encode(user.getPassword())));
 
         userRepository.saveAll(users);
     }
