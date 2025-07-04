1ffabedeee978b069f95b62d0158df0bf1998fab
Luka Lomidze <luka_lomidze@epam.com>
2025-07-04 20:12:44 +0400

    removed verbosity

--- src/main/java/ge/edu/cu/l_lomidze2/sabakalavro/controller/SecureController.java
+++ src/main/java/ge/edu/cu/l_lomidze2/sabakalavro/controller/SecureController.java
@@ -1,7 +1,6 @@
 package ge.edu.cu.l_lomidze2.sabakalavro.controller;
 
-import org.apache.logging.log4j.LogManager;
-import org.apache.logging.log4j.Logger;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -14,18 +13,11 @@ import ge.edu.cu.l_lomidze2.sabakalavro.service.UserService;
 @RestController
 @RequestMapping("/api/secure")
 public class SecureController {
-    private static Logger logger = LogManager.getLogger();
-
+    @Autowired
     private UserService userService;
-
-    public SecureController(UserService userService) {
-        this.userService = userService;
-    }
 
     @GetMapping("/user/{id}")
     public User getUser(@PathVariable Long id) {
-        logger.info("retrieved user");
-
         return userService.getUser(id);
     }
 }
