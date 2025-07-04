12126f7736486b8c3e15cd014f1ac15691523029
Luka Lomidze <luka_lomidze@epam.com>
2025-07-04 19:15:16 +0400

    added security

---
+++ src/main/java/ge/edu/cu/l_lomidze2/sabakalavro/config/SecurityConfig.java
@@ -0,0 +1,82 @@
+package ge.edu.cu.l_lomidze2.sabakalavro.config;
+
+import static org.springframework.security.config.Customizer.withDefaults;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.web.SecurityFilterChain;
+
+import ge.edu.cu.l_lomidze2.sabakalavro.repository.UserRepository;
+
+@Configuration
+public class SecurityConfig {
+    @Bean
+    SecurityFilterChain baseSecurity(
+        HttpSecurity http
+    ) throws Exception {
+        return http
+            .csrf(csrf -> csrf.disable())
+            .cors(withDefaults())
+            .authorizeHttpRequests(
+                requestRegistry -> requestRegistry
+                    .requestMatchers(HttpMethod.GET, "/user/login").permitAll()
+                    .requestMatchers(
+                        "/user/trainee",
+                        "/user/trainer"
+                    )
+                .permitAll()
+            )
+            .sessionManagement(
+                configurer -> configurer.sessionCreationPolicy(
+                    SessionCreationPolicy.STATELESS
+                )
+            )
+        .build();
+            // .addFilterBefore(authFilter, UsernamePasswordAuthenticationFilter.class)
+            // .addFilterBefore(bruteForceFilter, AuthenticationFilter.class);
+    }
+
+    @Bean
+    UserDetailsService userDetailsService() {
+        return new UserDetailsService() {
+            @Autowired
+            private UserRepository userRepository;
+
+            @Override
+            public UserDetails loadUserByUsername(
+                String username
+            ) throws UsernameNotFoundException {
+                var user = userRepository
+                    .findByUsername(username)
+                .orElseThrow(() -> new UsernameNotFoundException(username + " not found"));
+
+                return User.builder()
+                    .username(user.getUsername())
+                    .password(user.getPassword())
+                .build();
+            }
+        };
+    }
+
+    @Bean
+    AuthenticationManager authenticationManager(HttpSecurity http) throws Exception {
+        return http.getSharedObject(AuthenticationManagerBuilder.class).build();
+    }
+
+    @Bean
+    PasswordEncoder encoder() {
+        return new BCryptPasswordEncoder();
+    }
+}
