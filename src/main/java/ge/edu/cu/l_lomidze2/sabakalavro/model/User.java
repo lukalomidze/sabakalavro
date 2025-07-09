30881a362540cf8eb82610091204b4652ffe8945
Luka Lomidze <luka_lomidze@epam.com>
2025-07-09 17:45:20 +0400

    added required lombok annotations

--- src/main/java/ge/edu/cu/l_lomidze2/sabakalavro/model/User.java
+++ src/main/java/ge/edu/cu/l_lomidze2/sabakalavro/model/User.java
@@ -4,12 +4,16 @@ import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 @Entity
 @Builder
 @Data
+@NoArgsConstructor
+@AllArgsConstructor
 public class User {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
