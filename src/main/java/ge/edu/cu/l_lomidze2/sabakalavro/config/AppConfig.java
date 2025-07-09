969fb987abc619624c97b197f878744acc3c7193
Luka Lomidze <luka_lomidze@epam.com>
2025-07-09 16:36:26 +0400

    added missing bean annotation

--- src/main/java/ge/edu/cu/l_lomidze2/sabakalavro/config/AppConfig.java
+++ src/main/java/ge/edu/cu/l_lomidze2/sabakalavro/config/AppConfig.java
@@ -3,11 +3,13 @@ package ge.edu.cu.l_lomidze2.sabakalavro.config;
 import java.util.Map;
 import java.util.concurrent.ConcurrentHashMap;
 
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
 public class AppConfig {
-    public Map<String, Integer> cache() {
+    @Bean
+    Map<String, Integer> cache() {
         return new ConcurrentHashMap<>();
     }
 }
